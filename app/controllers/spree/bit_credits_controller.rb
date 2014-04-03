18cd8baf43d88520d04481ca0a5b85bd5e085710
InessaBarkan <inessabarkan@gmail.com>
2014-04-03 16:40:29 -0400

    remove get balance

--- app/controllers/spree/bit_credits_controller.rb
+++ app/controllers/spree/bit_credits_controller.rb
@@ -18,33 +18,33 @@ module Spree
 
     end
     def get_balance
-       token = cookies[:bitc]
-       render :js => "no cookie" if token.nil?
-
-       payment_method =  Spree::PaymentMethod.find(:first, :conditions => [ "lower(name) LIKE ?", '%bitcoin%' ])
-       
-       unless payment_method.nil?
-          begin
-              url  = URI.parse("#{payment_method.preferred_api_server}v1/accounts/token/#{token}")
-              Rails.logger.info(url)
-              http = Net::HTTP.new(url.host, url.port)
-              http.use_ssl = true
-              request = Net::HTTP::Get.new(url.path)
-              request.basic_auth(payment_method.preferred_key, "")
-              response = http.request(request)
-              Rails.logger.info(response.body)
-              result = JSON.parse(response.body)
-
-
-              if( result["status"] == "success")
-                session[:bitcredits_old_balance] = result['balance'].to_f  if session[:bitcredits_old_balance].blank?
-              end
-          rescue  Exception => ex
-            Rails.logger.info ex.to_s
-            render  :js => ex.to_s
-          end
-
-       end
+#       token = cookies[:bitc]
+#       render :js => "no cookie" if token.nil?
+#
+#       payment_method =  Spree::PaymentMethod.find(:first, :conditions => [ "lower(name) LIKE ?", '%bitcoin%' ])
+#
+#       unless payment_method.nil?
+#          begin
+#              url  = URI.parse("#{payment_method.preferred_api_server}v1/accounts/token/#{token}")
+#              Rails.logger.info(url)
+#              http = Net::HTTP.new(url.host, url.port)
+#              http.use_ssl = true
+#              request = Net::HTTP::Get.new(url.path)
+#              request.basic_auth(payment_method.preferred_key, "")
+#              response = http.request(request)
+#              Rails.logger.info(response.body)
+#              result = JSON.parse(response.body)
+#
+#
+#              if( result["status"] == "success")
+#                session[:bitcredits_old_balance] = result['balance'].to_f  if session[:bitcredits_old_balance].blank?
+#              end
+#          rescue  Exception => ex
+#            Rails.logger.info ex.to_s
+#            render  :js => ex.to_s
+#          end
+#
+#       end
        render  :js => "Saved"
     end
   end
