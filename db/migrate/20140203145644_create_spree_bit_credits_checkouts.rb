112f934377fcceb798e94f963c17ca90998a597d
Nima Shariatian <nima@onevio.org>
2014-07-24 12:00:10 -0400

    updates: migrate, ensure creating the tables if not exist

--- db/migrate/20140203145644_create_spree_bit_credits_checkouts.rb
+++ db/migrate/20140203145644_create_spree_bit_credits_checkouts.rb
@@ -1,7 +1,16 @@
 class CreateSpreeBitCreditsCheckouts < ActiveRecord::Migration
   def up
+    return if ActiveRecord::Base.connection.table_exists? 'spree_bit_credits_checkouts'
+
+    create_table :bit_credits_checkouts do |t|
+      t.float :old_balance
+      t.float :new_balance
+      t.string :source
+      t.integer :tx_id
+    end
   end
 
   def down
+    drop_table :bit_credits_checkouts
   end
 end
