1321e667f30a689f82609ba865ecf42931ef1818
Nima Shariatian <nima@onevio.org>
2014-07-24 12:30:59 -0400

    updates: migrations.. removes the checks

--- db/migrate/20140203145644_create_spree_bit_credits_checkouts.rb
+++ db/migrate/20140203145644_create_spree_bit_credits_checkouts.rb
@@ -1,16 +1,14 @@
 class CreateSpreeBitCreditsCheckouts < ActiveRecord::Migration
   def up
-    unless ActiveRecord::Base.connection.table_exists? 'spree_bit_credits_checkouts'
-      create_table :bit_credits_checkouts do |t|
-        t.float :old_balance
-        t.float :new_balance
-        t.string :source
-        t.integer :tx_id
-      end
+    create_table :bit_credits_checkouts do |t|
+      t.float :old_balance
+      t.float :new_balance
+      t.string :source
+      t.integer :tx_id
     end
   end
 
   def down
-    drop_table :bit_credits_checkouts if ActiveRecord::Base.connection.table_exists? 'spree_bit_credits_checkouts'
+    drop_table :bit_credits_checkouts
   end
 end
